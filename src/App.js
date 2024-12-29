3a97d1fbd4173c72dc104a794d7b28929f0c1aac
Sarah Ocy <sarahocy@hotmail.com>
2024-12-29 16:40:04 -0300

    Sessão 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./components/Header";
 import MainSection from "./components/MainSection";
+import AboutSection from "./components/AboutSection";
 import './App.css';
 
 function App() {
@@ -9,11 +10,13 @@ function App() {
       <div className="background-blur">
         <div className="blur-circle blur-top"></div>
         <div className="blur-circle blur-right1"></div>
+        <div className="blur-circle blur-left1"></div>
       </div>
       
       <>
         <Header />
         <MainSection />
+        <AboutSection />
       </>
     </div>
   );
---
+++ src/components/AboutSection.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../assets/styles/AboutSection.css";
+import moviesImage from "../assets/images/filmes.png"; 
+
+const AboutSection = () => {
+  return (
+    <section className="about-section">
+
+      <div className="image-container">
+        <img src={moviesImage} alt="Galeria de cenas" />
+      </div>
+
+     
+      <div className="about-text">
+        <h2>
+          O que <span className="highlight">somos?</span>
+        </h2>
+        <p>
+          Se você assiste a filmes ou séries e sempre <br/>
+          fica com vontade de experimentar as <br/>
+          comidas incríveis da telona, este <br/>
+          é o lugar certo!
+        </p>
+        <p>
+          Transforme cenas icônicas <br/>
+          em pratos reais e mate a curiosidade <br/>
+          (e a fome) de provar os sabores <br/>
+          do cinema!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
