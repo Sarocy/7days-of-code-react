7609a16e138b1f3a7e50a8ffdc3f1a0f5aad6cf0
Sarah Ocy <sarahocy@hotmail.com>
2024-12-28 22:41:30 -0300

    Adicionando o código inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Receitas de Filmes e Séries</h1>
+        <p>Explore os pratos mais famosos da cultura pop!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
