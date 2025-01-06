df955d9b1c7cde9b04b6501f971519f704c6feb8
Sarah Ocy <sarahocy@hotmail.com>
2025-01-06 14:32:36 -0300

    Sessão 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "./components/Header";
 import MainSection from "./components/MainSection";
 import AboutSection from "./components/AboutSection";
+import RecipesSection from "./components/RecipesSection";
 import './App.css';
 
 function App() {
@@ -11,12 +12,15 @@ function App() {
         <div className="blur-circle blur-top"></div>
         <div className="blur-circle blur-right1"></div>
         <div className="blur-circle blur-left1"></div>
+        <div className="blur-circle blur-right2"></div>
+        <div className="blur-circle blur-left2"></div>
       </div>
       
       <>
         <Header />
         <MainSection />
         <AboutSection />
+        <RecipesSection />
       </>
     </div>
   );
---
+++ src/components/RecipesSection.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "../assets/styles/RecipesSection.css";
+
+const recipes = [
+  {
+    id: 1,
+    name: "Lasanha de Garfield",
+    image: require("../assets/images/lasanha.png"),
+  },
+  {
+    id: 2,
+    name: "Cannoli de Poderoso Chefão",
+    image: require("../assets/images/cannoli.png"),
+  },
+  {
+    id: 3,
+    name: "Churros de Chaves",
+    image: require("../assets/images/churros.png"),
+  },
+  {
+    id: 4,
+    name: "Ratatouille de Ratatouille",
+    image: require("../assets/images/ratatouille.png"),
+  },
+  {
+    id: 5,
+    name: "Donuts de Simpsons",
+    image: require("../assets/images/donuts.png"),
+  },
+  {
+    id: 6,
+    name: "Waffles de Stranger Things",
+    image: require("../assets/images/waffles.png"),
+  },
+];
+
+const RecipesSection = () => {
+  return (
+    <section className="recipes-section">
+      <h2 className="section-title">Receitas</h2>
+      <div className="recipes-container">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="recipe-card">
+            <div className="recipe-image">
+              <img src={recipe.image} alt={recipe.name} />
+            </div>
+            <div className="recipe-details">
+              <h3>{recipe.name}</h3>
+              <button className="access-button">acessar</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default RecipesSection;
